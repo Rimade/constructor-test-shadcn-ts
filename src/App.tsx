6e3ba351fde75bbca4228134dacf19cdd12f3986
Muhammad-Amin <muhammad-amin4@mail.ru>
2024-12-15 22:40:17 +0300

    подключил shadcn tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Button } from './components/ui/button';
 
 const App = () => {
   return (
     <div>
-      <h1>Mindbox Test</h1>
+      <Button>Click me</Button>
     </div>
   );
 };
